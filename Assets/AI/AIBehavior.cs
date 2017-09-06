741e1596e0da753b3854bda83aa07495ef9dcffb
Nico Pampe <npampe@mines.edu>
2017-09-05 18:31:07 -0600

    AI: basic movement finished.

---
+++ Assets/AI/AIBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIBehavior : MonoBehaviour {
+	public float walkingSpeed = 5.0f;
+	public float rotationRange = 110.0f;
+
+	private Rigidbody _rigidbody;
+	private bool turning;
+
+	// Use this for initialization
+	void Start () {
+		turning = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, walkingSpeed * Time.deltaTime);
+
+		int delay = 2;
+		if (System.DateTime.Now.Second % delay == 0 && !turning) {
+			turning = true;
+			float angle = Random.Range(-rotationRange, rotationRange);
+			transform.Rotate(0, angle, 0);
+		} else if (System.DateTime.Now.Second % delay == 1) {
+			turning = false;
+		}
+
+
+	}
+}
