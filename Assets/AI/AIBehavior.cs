88288f60ab6d04fbacfc05301177d8cb8d5c3ec5
Nico Pampe <npampe@mines.edu>
2017-09-05 19:04:41 -0600

    AI: Doesn't walk out of the world. Added walls

--- Assets/AI/AIBehavior.cs
+++ Assets/AI/AIBehavior.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class AIBehavior : MonoBehaviour {
 	public float walkingSpeed = 5.0f;
 	public float rotationRange = 110.0f;
+	public float obstacleRange = 1.0f;
 
 	private Rigidbody _rigidbody;
 	private bool turning;
@@ -16,6 +17,16 @@ public class AIBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (0, 0, walkingSpeed * Time.deltaTime);
+
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+//		Debug.Log (hit.distance);
+		if (Physics.SphereCast(ray, 0.75f, out hit)) {
+			if (hit.distance < obstacleRange) {
+				float angle = Random.Range(-rotationRange, rotationRange);
+				transform.Rotate(0, angle, 0);
+			}
+		}
 
 		int delay = 2;
 		if (System.DateTime.Now.Second % delay == 0 && !turning) {
