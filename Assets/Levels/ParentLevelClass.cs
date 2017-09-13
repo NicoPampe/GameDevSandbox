d4d0f6cb68f8b75baa9dbdb303363f36569cf428
Nico Pampe <npampe@mines.edu>
2017-09-12 18:21:18 -0600

    lvlSettup: Player can walk through a door and the lvl will start.

---
+++ Assets/Levels/ParentLevelClass.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParentLevelClass : MonoBehaviour {
+	public float playerSpeed = 1.0f;	
+
+	private GameObject _player;
+	private PlayerBehavior _playerBehavior;
+	private bool runningLevel = false;
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.Find ("/Player");
+		_playerBehavior = _player.GetComponent<PlayerBehavior> ();
+
+		if (playerSpeed == 0) {
+			playerSpeed = 1.0f;
+		}
+		playerSpeed *= _playerBehavior.walkingSpeed / 50.0f;
+	}
+	
+	public void StartLevel (Collider collider) {
+		runningLevel = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (runningLevel) {
+			// TODO: use the player movement rather than just transforming their pos
+			_player.transform.Translate(new Vector3(0, 0, playerSpeed), gameObject.transform);
+		}
+	}
+}
---
+++ Assets/Levels/levelTriggers.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelTriggers : MonoBehaviour {
+	void OnTriggerEnter(Collider c) {
+		gameObject.GetComponentInParent<levelBehavior> ().StartLevel (c);
+	}
+}
